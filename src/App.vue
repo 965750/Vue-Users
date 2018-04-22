0c9ae5f6360f8507822f125a8626776cd3cae03f
Adrian Zajac <aventhelast@gmail.com>
2018-04-23 01:12:04 +0200

    http request

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      users: []
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.$http.get('https://api.github.com/users').then(function(data){
+      console.log(data.body);
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
